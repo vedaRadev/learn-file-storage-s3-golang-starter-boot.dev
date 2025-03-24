f9741f012acd546ee88cdc3890e8322f5e0672e4
vedaRadev <ryancarmstrong97@gmail.com>
2025-03-24 14:34:41 -0400

    prefix video uploads with portrait/landscape depending on aspect ratio

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -9,11 +9,43 @@ import (
     "context"
     "crypto/rand"
     "strings"
+    "os/exec"
+    "bytes"
+    "encoding/json"
+    "math"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+func getVideoAspectRatio(filePath string) (string, error) {
+    cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+    stdoutBuf := bytes.NewBuffer([]byte{})
+    cmd.Stdout = stdoutBuf
+    err := cmd.Run();
+    if err != nil { return "", err }
+    jsonOut := make(map[string]any)
+    err = json.Unmarshal(stdoutBuf.Bytes(), &jsonOut)
+    if err != nil { return "", err }
+
+    streams := jsonOut["streams"].([]any)
+    firstStream := streams[0].(map[string]any)
+    width := firstStream["width"].(float64)
+    height := firstStream["height"].(float64)
+
+    ratio := width / height
+    var result string
+    if math.Abs(ratio - 1.7777) < 0.01 {
+        result = "landscape" // 16:9
+    } else if math.Abs(ratio - 0.5625) < 0.01 {
+        result = "portrait" // 9:16
+    } else {
+        result = "other"
+    }
+
+    return result, nil
+}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
     videoIDString := r.PathValue("videoID")
@@ -57,7 +89,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
         respondWithError(w, http.StatusInternalServerError, "failed to create temp file", nil)
         return
     }
-    defer os.Remove("tubely-upload.mp4");
+    defer os.Remove(tempFile.Name());
     defer tempFile.Close()
 
     maxBytesReader := http.MaxBytesReader(w, videoFile, 1 << 30)
@@ -74,9 +106,16 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
         return
     }
 
+    fmt.Printf("%s\n", tempFile.Name());
+    aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "failed to get video aspect ratio", err)
+        return
+    }
+
     byteSlice := make([]byte, 32, 32)
     rand.Read(byteSlice)
-    fileKey := fmt.Sprintf("%x.%s", byteSlice, strings.Split(mediaType, "/")[1])
+    fileKey := fmt.Sprintf("%s/%x.%s", aspectRatio, byteSlice, strings.Split(mediaType, "/")[1])
     fmt.Printf("%s\n", fileKey)
     fmt.Printf("%s\n", mediaType)
     input := s3.PutObjectInput {
